3fbdf72e8120611b244422831b3c0821470006e1
irfanuddinahmad <iahmad@2u.com>
2026-04-30 19:51:06 +0500

    fix: fixed tests

--- cms/djangoapps/contentstore/views/tests/test_exam_settings_view.py
+++ cms/djangoapps/contentstore/views/tests/test_exam_settings_view.py
@@ -26,7 +26,6 @@ from common.djangoapps.util.testing import UrlResetMixin
 )
 @override_waffle_flag(toggles.LEGACY_STUDIO_CERTIFICATES, True)
 @override_waffle_flag(toggles.LEGACY_STUDIO_SCHEDULE_DETAILS, True)
-@override_waffle_flag(toggles.LEGACY_STUDIO_CONFIGURATIONS, True)
 @override_waffle_flag(toggles.LEGACY_STUDIO_GRADING, True)
 @override_waffle_flag(toggles.LEGACY_STUDIO_ADVANCED_SETTINGS, True)
 class TestExamSettingsView(CourseTestCase, UrlResetMixin):
